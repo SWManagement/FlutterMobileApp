c94ff71e140249750f25a431b545df26e76b9198
Martin Strekelj <martin.strekelj123@gmail.com>
2020-05-06 15:55:50 +0200

    Skeniranje ene kode po uspesnem skeniranju ali cancel crasha app

--- lib/Skeniraj.dart
+++ lib/Skeniraj.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class Skeniraj extends StatefulWidget {
     const Skeniraj({
@@ -9,8 +11,48 @@ class Skeniraj extends StatefulWidget {
 }
 
 class _SkenirajState extends State<Skeniraj> {
+  String _scanBarcode = "Unknown";
+
+  @override
+  void initState() { 
+    super.initState();
+  }
+
+startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            "#ff6666", "Cancel", true, ScanMode.BARCODE)
+        .listen((barcode){
+          print(barcode);
+        });
+  }
+
+Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("SKENIRAJ"),);
+    return Column(children: <Widget>[
+      Text("$_scanBarcode"),
+      RaisedButton(
+      onPressed: () => scanBarcodeNormal(),
+      child: Text("Start barcode scan")),
+      
+      RaisedButton(onPressed: () => startBarcodeScanStream(), child: Text("Barcode stream"),),
+    ],);
   }
 }
